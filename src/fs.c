b440b338fb0d13526dad45b4bfb83e96cf2b6063
miso <green-24@outlook.jp>
2026-03-29 01:30:35 +0900

    impl: encfs_write and encfs_read

--- src/fs.c
+++ src/fs.c
@@ -1,6 +1,8 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -47,6 +49,8 @@ static void *encfs_init(struct fuse_conn_info *conn, struct fuse_config *cfg) {
 }
 
 static int encfs_getattr(const char *path, struct stat *stbuf, struct fuse_file_info *fi) {
+    (void)fi;
+
     int res;
     encfs_state_t *state = fuse_get_context()->private_data;
 
@@ -64,6 +68,10 @@ static int encfs_getattr(const char *path, struct stat *stbuf, struct fuse_file_
 }
 
 static int encfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi, enum fuse_readdir_flags flags) {
+    (void)offset;
+    (void)fi;
+    (void)flags;
+
     DIR *dp;
     struct dirent *de;
     encfs_state_t *state = fuse_get_context()->private_data;
@@ -121,7 +129,13 @@ static int encfs_create(const char *path, mode_t mode, struct fuse_file_info *fi
 
     ENCFS_PATH(encfs_path, state, path);
 
-    fd = open(encfs_path, fi->flags, mode);
+    int flags = fi->flags;
+    if ((flags & O_ACCMODE) == O_WRONLY) {
+        flags = (flags & ~O_ACCMODE) | O_RDWR;
+    }
+    flags &= ~O_APPEND;
+
+    fd = open(encfs_path, flags, mode);
     if (fd == -1) {
         return -errno;
     }
@@ -146,25 +160,31 @@ static int encfs_create(const char *path, mode_t mode, struct fuse_file_info *fi
 
 static int encfs_open(const char *path, struct fuse_file_info *fi) {
     uint8_t salt[ENCFS_SALT_SIZE];
-    uint8_t header[ENCFS_HEADER_SIZE];
+    uint8_t header[ENCFS_PREAMBLE_SIZE];
     int fd, n;
 
     encfs_state_t *state = (encfs_state_t*)fuse_get_context()->private_data;
 
     ENCFS_PATH(encfs_path, state, path);
 
-    fd = open(encfs_path, fi->flags);
+    int flags = fi->flags;
+    if ((flags & O_ACCMODE) == O_WRONLY) {
+        flags = (flags & ~O_ACCMODE) | O_RDWR;
+    }
+    flags &= ~O_APPEND;
+
+    fd = open(encfs_path, flags);
     if (fd == -1) {
         return -errno;
     }
 
-    read(fd, header, ENCFS_HEADER_SIZE);
-    if (memcmp(header, ENCFS_MAGIC, ENCFS_MAGIC_SIZE) != 0 || header[ENCFS_VERSION_SIZE] != ENCFS_VERSION) {
+    n = pread(fd, header, ENCFS_PREAMBLE_SIZE, 0);
+    if (memcmp(header, ENCFS_MAGIC, ENCFS_MAGIC_SIZE) != 0 || header[ENCFS_MAGIC_SIZE] != ENCFS_VERSION) {
         close(fd);
         return -EIO;
     }
 
-    n = read(fd, salt, ENCFS_SALT_SIZE);
+    n = pread(fd, salt, ENCFS_SALT_SIZE, ENCFS_PREAMBLE_SIZE);
     if (n != ENCFS_SALT_SIZE) {
         close(fd);
         return -EIO;
@@ -174,6 +194,8 @@ static int encfs_open(const char *path, struct fuse_file_info *fi) {
 }
 
 static int encfs_release(const char *path, struct fuse_file_info *fi) {
+    (void)path;
+
     encfs_file_t *file = (encfs_file_t*)fi->fh;
     if (file != NULL) {
         memset(file->key, 0, ENCFS_KEY_SIZE);
@@ -184,23 +206,141 @@ static int encfs_release(const char *path, struct fuse_file_info *fi) {
 }
 
 static int encfs_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
+    uint8_t nonce[ENCFS_NONCE_SIZE];
+    uint8_t tag[ENCFS_TAG_SIZE];
+    uint8_t plain[ENCFS_BLOCK_SIZE];
+    uint8_t enc[ENCFS_BLOCK_SIZE];
+    uint8_t enc_block[ENCFS_ENC_BLOCK_SIZE];
     encfs_file_t *file = (encfs_file_t*)fi->fh;
     encfs_state_t *state = (encfs_state_t*)fuse_get_context()->private_data;
+    struct stat st;
+    int start_block, num_blocks;
+    int raw_num_blocks, raw_offset;
+    size_t raw_size;
+    size_t bytes_written = 0;
 
     ENCFS_PATH(encfs_path, state, path);
 
-    // unimplemented
+    lstat(encfs_path, &st);
+    raw_size = calc_raw_file_size(st.st_size);
+    raw_num_blocks = (raw_size + ENCFS_BLOCK_SIZE - 1) / ENCFS_BLOCK_SIZE;
 
-    return 0;
+    start_block = offset / ENCFS_BLOCK_SIZE;
+    num_blocks = ((offset % ENCFS_BLOCK_SIZE) + size + ENCFS_BLOCK_SIZE - 1) / ENCFS_BLOCK_SIZE;
+
+    for (int i = 0; i < num_blocks; i++) {
+        int block_start = (i == 0) ? offset % ENCFS_BLOCK_SIZE : 0;
+        int block_end = (i == num_blocks - 1) ? ((offset + size - 1) % ENCFS_BLOCK_SIZE) + 1 : ENCFS_BLOCK_SIZE;
+        int block_idx = start_block + i;
+        bool in_partial_block = (raw_size != 0) && ((block_start != 0) || (block_end != ENCFS_BLOCK_SIZE));
+        int write_len = block_end - block_start;
+        int plain_len = 0;
+        int enc_len, enc_size;
+
+        raw_offset = ENCFS_HEADER_SIZE + block_idx * ENCFS_ENC_BLOCK_SIZE;
+        memset(plain, 0, ENCFS_BLOCK_SIZE);
+
+        if (in_partial_block) {
+            uint8_t enc_buf[ENCFS_ENC_BLOCK_SIZE];
+            bool is_last_block = i == raw_num_blocks - 1;
+            size_t raw_data_size = (is_last_block) ? raw_size % ENCFS_BLOCK_SIZE : ENCFS_BLOCK_SIZE;
+            size_t enc_size = raw_data_size + ENCFS_NONCE_SIZE + ENCFS_TAG_SIZE;
+            pread(file->fd, enc_buf, enc_size, raw_offset);
+
+            uint8_t *nonce_ptr = enc_buf;
+            uint8_t *tag_ptr = enc_buf + ENCFS_NONCE_SIZE;
+            uint8_t *enc_ptr = tag_ptr + ENCFS_TAG_SIZE;
+
+            crypto_aes_decrypt(file->key, nonce_ptr, tag_ptr, enc_ptr, raw_data_size, plain, &plain_len);
+        }
+
+        memcpy(plain + block_start, buf + bytes_written, write_len);
+        if (block_start + write_len > plain_len) {
+            plain_len = block_start + write_len;
+        }
+
+        crypto_getrandom(nonce, ENCFS_NONCE_SIZE);
+        crypto_aes_encrypt(file->key, nonce, plain, plain_len, enc, &enc_len, tag);
+
+        memcpy(enc_block, nonce, ENCFS_NONCE_SIZE);
+        memcpy(enc_block + ENCFS_NONCE_SIZE, tag, ENCFS_TAG_SIZE);
+        memcpy(enc_block + ENCFS_NONCE_SIZE + ENCFS_TAG_SIZE, enc, enc_len);
+
+        enc_size = ENCFS_NONCE_SIZE + ENCFS_TAG_SIZE + enc_len;
+        pwrite(file->fd, enc_block, enc_size, raw_offset);
+
+        bytes_written += write_len;
+    }
+
+    return bytes_written;
 }
 
 static int encfs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
+    uint8_t enc_buf[ENCFS_ENC_BLOCK_SIZE];
+    uint8_t plain[ENCFS_BLOCK_SIZE];
+
     encfs_file_t *file = (encfs_file_t*)fi->fh;
     encfs_state_t *state = (encfs_state_t*)fuse_get_context()->private_data;
+    struct stat st;
+    int start_block, num_blocks;
+    int raw_num_blocks, raw_offset;
+    size_t raw_size;
+    size_t bytes_read = 0;
 
     ENCFS_PATH(encfs_path, state, path);
 
-    // unimplemented
+    lstat(encfs_path, &st);
+    raw_size = calc_raw_file_size(st.st_size);
+    raw_num_blocks = (raw_size + ENCFS_BLOCK_SIZE - 1) / ENCFS_BLOCK_SIZE;
+
+    if (size > raw_size) {
+        size = raw_size;
+    }
+
+    start_block = offset / ENCFS_BLOCK_SIZE;
+    num_blocks = ((offset % ENCFS_BLOCK_SIZE) + size + ENCFS_BLOCK_SIZE - 1) / ENCFS_BLOCK_SIZE;
+
+    for (int i = 0; i < num_blocks; i++) {
+        int block_start = (i == 0) ? offset % ENCFS_BLOCK_SIZE : 0;
+        int block_end = (i == num_blocks - 1) ? ((offset + size - 1) % ENCFS_BLOCK_SIZE) + 1 : ENCFS_BLOCK_SIZE;
+        int block_idx = start_block + i;
+        bool is_last_block = i == raw_num_blocks - 1;
+        size_t read_len = (is_last_block) ? block_end - block_start : ENCFS_BLOCK_SIZE;
+        size_t enc_data_len = read_len + ENCFS_NONCE_SIZE + ENCFS_TAG_SIZE;
+        int plain_len;
+
+        raw_offset = ENCFS_HEADER_SIZE + block_idx * ENCFS_ENC_BLOCK_SIZE;
+
+        pread(file->fd, enc_buf, enc_data_len, raw_offset);
+
+        uint8_t *nonce_ptr = enc_buf;
+        uint8_t *tag_ptr = enc_buf + ENCFS_NONCE_SIZE;
+        uint8_t *enc_ptr = enc_buf + ENCFS_NONCE_SIZE + ENCFS_TAG_SIZE;
+
+        crypto_aes_decrypt(file->key, nonce_ptr, tag_ptr, enc_ptr, read_len, plain, &plain_len);
+
+        memcpy(buf + bytes_read, plain + block_start, read_len);
+        bytes_read += read_len;
+    }
+
+    return bytes_read;
+}
+
+static int encfs_truncate(const char *path, off_t size, struct fuse_file_info *fi) {
+    encfs_file_t *file = (encfs_file_t*)fi->fh;
+    encfs_state_t *state = (encfs_state_t*)fuse_get_context()->private_data;
+    int res;
+
+    ENCFS_PATH(encfs_path, state, path);
+
+    /*
+     * TODO: impl
+     */
+
+    res = truncate(encfs_path, size);
+    if (res == -1) {
+        return -errno;
+    }
 
     return 0;
 }
@@ -214,4 +354,5 @@ struct fuse_operations encfs_ops = {
     .write = encfs_write,
     .read = encfs_read,
     .release = encfs_release,
+    .truncate = encfs_truncate,
 };
